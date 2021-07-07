497758c5a1e34cdb521000c39f872f625b73bb0f
KubanovValentin <delo.biss13@gmail.com>
2021-07-07 21:55:48 +0300

    add fifth-screen block

--- src/js/script.js
+++ src/js/script.js
@@ -1,47 +1,47 @@
 'use strict';
 // --------------------------------1 first-page анимация
-const container = document.querySelector('.container');
-for (var i = 0; i<= 400; i++){
-    const blocks = document.createElement('div');
-    blocks.classList.add('block');
-    container.appendChild(blocks);
-}
+// const container = document.querySelector('.container');
+// for (var i = 0; i<= 200; i++){
+//     const blocks = document.createElement('div');
+//     blocks.classList.add('block');
+//     container.appendChild(blocks);
+// }
 
-function animateBlocks() {
-    anime({
-        targets: '.block',
-        translateX: function(){
-            return anime.random(-700, 700);
-        },
-        translateY: function(){
-            return anime.random(-500, 500);
-        },
-        scaleX: function(){
-            return anime.random(-1.5, 1.5);
-        },
-        scaleY: function(){
-            return anime.random(-1.5, 1.5);
-        },
+// function animateBlocks() {
+//     anime({
+//         targets: '.block',
+//         translateX: function(){
+//             return anime.random(-700, 700);
+//         },
+//         translateY: function(){
+//             return anime.random(-500, 500);
+//         },
+//         scaleX: function(){
+//             return anime.random(-1.5, 1.5);
+//         },
+//         scaleY: function(){
+//             return anime.random(-1.5, 1.5);
+//         },
 
-        easing: 'linear',
-        duration: 3000,
-        delay: anime.stagger(10),
-        complete: animateBlocks,
-    });
-}
+//         easing: 'linear',
+//         duration: 3000,
+//         delay: anime.stagger(10),
+//         complete: animateBlocks,
+//     });
+// }
 
-animateBlocks();
+// animateBlocks();
 
-// -----------------------кнопка перехода на другой экран
-function hideshow() {
-    document.getElementById("first").style = "display:none";
-    document.getElementById("second").style = "display:block";
-}
+// // -----------------------кнопка перехода на другой экран
+// function hideshow() {
+//     document.getElementById("first").style = "display:none";
+//     document.getElementById("second").style = "display:block";
+// }
 
-function hideShow() {
-    document.getElementById("second").style = "display:none";
-    document.getElementById("common").style = "display:block";
-}
+// function hideShow() {
+//     document.getElementById("second").style = "display:none";
+//     document.getElementById("common").style = "display:block";
+// }
 
 
 // ----------------
@@ -49,11 +49,11 @@ function hideShow() {
 // second page 
 // window.addEventListener("load", function() {
 
-	// const text = document.querySelector('.third-screen__text');
+	// const text = document.querySelector('.fifth-screen__text');
     // text.innerHTML = text.textContent.replace(/\S/g, "<span>$&</span>");
 
     // const animation = anime.timeline({
-    //     targets : '.third-screen__text span',
+    //     targets : '.fifth-screen__text span',
     //     easing : 'easeInOutExpo',
     //     loop : true,
     // });
@@ -64,7 +64,7 @@ function hideShow() {
     //     translateX : 0,
     //     translateY : 0,
     //     duration: 1000,
-    //     delay: anime.stagger(1),
+    //     delay: anime.stagger(100000),
     // })
     // .add({
     //     rotate : function(){
@@ -89,6 +89,6 @@ function hideShow() {
     // });
 // });
 
-// -----------------------smoke  
 
+// 
 
