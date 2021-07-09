c39a44bbd9d8fb2155c36fc48e4d43edb84e2c1d
KubanovValentin <delo.biss13@gmail.com>
2021-07-09 19:18:14 +0300

    add _sixth-screen

--- src/js/script.js
+++ src/js/script.js
@@ -1,47 +1,47 @@
 'use strict';
 // --------------------------------1 first-page анимация
-// const container = document.querySelector('.container');
-// for (var i = 0; i<= 200; i++){
-//     const blocks = document.createElement('div');
-//     blocks.classList.add('block');
-//     container.appendChild(blocks);
-// }
+const container = document.querySelector('.container');
+for (var i = 0; i<= 200; i++){
+    const blocks = document.createElement('div');
+    blocks.classList.add('block');
+    container.appendChild(blocks);
+}
 
-// function animateBlocks() {
-//     anime({
-//         targets: '.block',
-//         translateX: function(){
-//             return anime.random(-700, 700);
-//         },
-//         translateY: function(){
-//             return anime.random(-500, 500);
-//         },
-//         scaleX: function(){
-//             return anime.random(-1.5, 1.5);
-//         },
-//         scaleY: function(){
-//             return anime.random(-1.5, 1.5);
-//         },
+function animateBlocks() {
+    anime({
+        targets: '.block',
+        translateX: function(){
+            return anime.random(-700, 700);
+        },
+        translateY: function(){
+            return anime.random(-500, 500);
+        },
+        scaleX: function(){
+            return anime.random(-1.5, 1.5);
+        },
+        scaleY: function(){
+            return anime.random(-1.5, 1.5);
+        },
 
-//         easing: 'linear',
-//         duration: 3000,
-//         delay: anime.stagger(10),
-//         complete: animateBlocks,
-//     });
-// }
+        easing: 'linear',
+        duration: 3000,
+        delay: anime.stagger(10),
+        complete: animateBlocks,
+    });
+}
 
-// animateBlocks();
+animateBlocks();
 
 // // -----------------------кнопка перехода на другой экран
-// function hideshow() {
-//     document.getElementById("first").style = "display:none";
-//     document.getElementById("second").style = "display:block";
-// }
+function hideshow() {
+    document.getElementById("first").style = "display:none";
+    document.getElementById("second").style = "display:block";
+}
 
-// function hideShow() {
-//     document.getElementById("second").style = "display:none";
-//     document.getElementById("common").style = "display:block";
-// }
+function hideShow() {
+    document.getElementById("second").style = "display:none";
+    document.getElementById("common").style = "display:block";
+}
 
 
 // ----------------
@@ -49,46 +49,59 @@
 // second page 
 // window.addEventListener("load", function() {
 
-	// const text = document.querySelector('.fifth-screen__text');
-    // text.innerHTML = text.textContent.replace(/\S/g, "<span>$&</span>");
+	const text = document.querySelector('.fifth-screen__textAct');
+    text.innerHTML = text.textContent.replace(/\S/g, "<span>$&</span>");
 
-    // const animation = anime.timeline({
-    //     targets : '.fifth-screen__text span',
-    //     easing : 'easeInOutExpo',
-    //     loop : true,
-    // });
+    const animation = anime.timeline({
+        targets : '.fifth-screen__textAct span',
+        easing : 'easeInOutExpo',
+        loop : true,
+    });
 
-    // animation
-    // .add({
-    //     rotate :0,
-    //     translateX : 0,
-    //     translateY : 0,
-    //     duration: 1000,
-    //     delay: anime.stagger(100000),
-    // })
-    // .add({
-    //     rotate : function(){
-    //         return anime.random(-360, 360);
-    //     },
-    //     translateX : function(){
-    //         return anime.random(-500, 500);
-    //     },
-    //     translateY : function(){
-    //         return anime.random(-500, 500);
-    //     },
-    //     duration: 5000,
-    //     delay: anime.stagger(20),
-    // })
+    animation
+    .add({
+        rotate :0,
+        translateX : 0,
+        translateY : 0,
+        duration: 1000,
+        delay: anime.stagger(1),
+    })
+    .add({
+        rotate : function(){
+            return anime.random(-360, 360);
+        },
+        translateX : function(){
+            return anime.random(-500, 500);
+        },
+        translateY : function(){
+            return anime.random(-500, 500);
+        },
+        duration: 5000,
+        delay: anime.stagger(20),
+    })
 
-    // .add({
-    //     rotate :0,
-    //     translateX : 0,
-    //     translateY : 0,
-    //     duration: 5000,
-    //     delay: anime.stagger(20),
-    // });
+    .add({
+        rotate :0,
+        translateX : 0,
+        translateY : 0,
+        duration: 5000,
+        delay: anime.stagger(20),
+    });
 // });
 
 
 // 
 
+function changeBg() {
+    document.querySelector(".fifth-screen__heart ").style.background = "#ff2020";
+    
+}
+
+VanillaTilt.init(document.querySelectorAll(".sixth-screen__slip"),{
+    max: 50,
+    speed: 400,
+    glare:true,
+    // max-glare : 1,
+    // "glare-prerender":true,
+});
+VanillaTilt.init(document.querySelectorAll(".sixth-screen__slip"));
